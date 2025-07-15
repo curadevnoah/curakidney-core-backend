42e1a101b1ccb8815bd3846f905a1679a7f0ca7f
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-07-15 13:17:40 +0800

    removed unused codes

--- src/_app/app.service.ts
+++ src/_app/app.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
-
   healthcheck() {
-    const nodeEnv = this.configService.get<string>('NODE_ENV');
-
     return {
       is_online: true,
     };
